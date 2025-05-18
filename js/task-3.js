c0fa2abce64b36a8396c578b68ffed23738d7c1a
Anna Pastykhova <rainbow261@gmail.com>
2025-05-18 20:01:05 +0300

    hw

---
+++ js/task-3.js
@@ -0,0 +1,13 @@
+const input = document.querySelector('#name-input');
+const output = document.querySelector('#name-output');
+
+input.addEventListener('input', () => {
+    const trimmedValue = input.value.trim();
+    input.value = trimmedValue;
+
+    if (trimmedValue === '') {
+        output.textContent = 'Anonymous';
+    } else {
+        output.textContent = trimmedValue;
+    }
+});
